306adb4ea76fa8db90ad518b280996cfa8cca29e
narayanvyas <narayanvyas87@gmail.com>
2018-11-23 19:32:59 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# IR Remote With Servo Motor
+Arduino IR Remote With Servo Motor.
+
+## Prerequisites
+1. Arduino Uno
+2. IR Receiver Module
+3. IR Remote
+4. Jumper Wires
+5. Breadboard / PCB
+6. Servo Motor
+7. 2 LEDs
+8. 2 x 220 Ohm Resistors
+
+## Breadboard Diagram -
+![Breadboard Diagram](https://github.com/narayanvyas/Arduino-IR-Remote-With-Servo-Motor/blob/master/Breadboard%20Diagram.jpeg)
+
+## Circuit Diagram -
+![Circuit Diagram](https://github.com/narayanvyas/Arduino-IR-Remote-With-Servo-Motor/blob/master/Circuit%20Diagram.jpeg)
+
+## Authors
+[Narayan Vyas](https://www.narayanvyas.org) - [Web Dev Fusion](https://www.webdevfusion.com)
+
+## License
+This project is licensed under the MIT License - see the LICENSE.md file for details.
+
+## Acknowledgments
+Thanks to [Peter Dalmaris](https://github.com/futureshocked) for the inspiriation.
---
+++ Source_Code/Source_Code.ino
@@ -0,0 +1,140 @@
+/*  IR Remote With Servo Motor
+ * 
+ *  GitHub URL - https://github.com/narayanvyas/Arduino-IR-Remote-With-Servo-Motor
+ * 
+ *  Developed By Web Dev Fusion
+ *  URL - https://www.webdevfusion.com84485 *  
+ * Components
+ * ----------
+ *  - Arduino Uno
+ *  - IR Receiver Module
+ *  - IR Remote
+ *  - Jumper Wires
+ *  - Breadboard / PCB
+ *  - Servo Motor
+ *  - 2 LEDs
+ *  - 2 x 220 Ohm Resistors
+ *  
+ *  Libraries
+ *  ---------
+ *  - IRRemote - https://github.com/z3t0/Arduino-IRremote (Search In Library Manager)
+ *  
+ */
+
+#include "IRremote.h"
+#include <Servo.h>
+
+int receiver = 11; // Signal Pin of IR receiver to Arduino Digital Pin 11
+int servoPin = 10;       // Servo Pin
+Servo Servo1;  
+
+IRrecv irrecv(receiver);     // create instance of 'irrecv'
+decode_results results;      // create instance of 'decode_results'
+
+void setup()   /*----( SETUP: RUNS ONCE )----*/
+{
+  Serial.begin(9600);
+  Serial.println("IR Receiver Button Decode"); 
+  irrecv.enableIRIn(); // Start the receiver
+  Servo1.attach(servoPin);
+  pinMode(6, OUTPUT);    //Red LED
+  pinMode(7, OUTPUT);     //Green LED
+}
+
+void loop()
+{
+  if (irrecv.decode(&results)) // have we received an IR signal?
+  {
+    translateIR(); 
+    irrecv.resume(); // receive the next value
+  }  
+}
+
+void translateIR() // takes action based on IR code received
+// describing Remote IR codes 
+
+{
+  switch(results.value)
+  {
+    case 16753245:
+      Serial.println("1 Pressed");
+      Servo1.write(20);
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      break;
+    case 16736925:
+      Serial.println("2 Pressed");
+      Servo1.write(40);
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      break;
+    case 16769565:
+    case 4294967295:
+      Serial.println("3 Pressed");
+      Servo1.write(60);
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      break;
+    case 16720605:
+      Serial.println("4 Pressed");
+      Servo1.write(80);
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      break;
+    case 16712445:
+      Serial.println("5 Pressed");
+      Servo1.write(100);
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      break;
+    case 16761405:
+      Serial.println("6 Pressed");
+      Servo1.write(120);
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      break;
+    case 16769055:
+      Serial.println("7 Pressed");
+      Servo1.write(140);
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      break;
+    case 16754775:
+    case 2747854299:
+    case 255:
+      Serial.println("8 Pressed");
+      Servo1.write(160);
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      break;
+    case 16748655:
+      Serial.println("9 Pressed");
+      Servo1.write(180);
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      break;
+    case 16750695:
+      Serial.println("0 Pressed");
+      Servo1.write(0);
+      digitalWrite(7, HIGH);
+      delay(100);
+      digitalWrite(7, LOW);
+      break;
+    default: 
+      Serial.println("Wrong Button");
+      digitalWrite(6, HIGH);
+      delay(100);
+      digitalWrite(6, LOW);
+      break;
+  }
+  delay(500);
+} 
